0d5f390aca10aee73106904121bb0579fc15d664
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-12-20 12:20:44 +0700

    Add project files.

---
+++ ClientRest_062_Annisa Dian Amarta/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRest_062_Annisa_Dian_Amarta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
